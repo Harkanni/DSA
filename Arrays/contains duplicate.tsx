6970ceb1bf4a3f399f33b9bdec3829369cf8ea00
harkanni <harkanzyempire001@gmail.com>
2025-01-06 22:50:30 +0100

    array: contains duplicate

--- Arrays/contains duplicate.tsx
+++ Arrays/contains duplicate.tsx
@@ -12,18 +12,36 @@ const arr = [1, 2, 3, 4, 1]
 // } // Time = O(n2); space = O(1)
 
 
+// const containsDuplicate = (arr) => {
+//    var hash = {};
+//    for (var i = 0; i < arr.length; i++) {
+//       if (hash[arr[i]] === true) {
+//          return true;
+//       } else {
+//          hash[arr[i]] = true;
+//       }
+//    }
+//    return false
+// } // Time = O(n); space = O(n);
+
+
 const containsDuplicate = (arr) => {
-   var hash = {};
-   for (var i = 0; i < arr.length; i++) {
-      if (hash[arr[i]] === true) {
-         return true;
-      } else {
-         hash[arr[i]] = true;
+   for(var i=0; i<arr.length; i++) {
+      let idx = Math.abs(arr[i] - 1)
+
+      arr[idx] = Math.abs(arr[i]) * -1
+      console.log(arr)
+   }
+
+
+   for(var i=0; i<arr.length; i++) {
+      if(arr[i] > 0) {
+         return true
       }
    }
-   return false
-} // Time = O(n); space = O(n);
 
+   return false
+} // Time = O(n) Space = O(1)
 
 
 console.log(containsDuplicate(arr))
